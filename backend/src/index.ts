82f753dea3603227df4f7a0d66ebe96d053d2ad7
Myeruyert <myeruyertjanibyek@gmail.com>
2024-10-22 12:41:09 +0800

    backend setup

---
+++ backend/src/index.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+
+//middlewares
+
+app.use(cors());
+
+app.use(express.json());
+
+const PORT: string = process.env.PORT || "";
+
+const MONGO_URI = process.env.MONGO_URI || "";
+
+app.get("/", async (req: Request, res: Response) => {
+  res.send("Welcome E-commerce API server");
+});
+
+//Server on
+
+app.listen(8000, () => {
+  console.log(`АPI Server is running on port:8000 `);
+});
