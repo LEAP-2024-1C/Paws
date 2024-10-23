5802057e45f3308d2c68d0c52d18de281f660827
Dwjrgal <dwjrgl651@gmail.com>
2024-10-23 10:02:38 +0800

    adoption cards

--- frontend/src/components/main_page/adoption-card.tsx
+++ frontend/src/components/main_page/adoption-card.tsx
@@ -1,14 +1,22 @@
 import Image from 'next/image'
 import React from 'react'
+import { FaArrowRight } from "react-icons/fa6";
 
-const PetsCard = ({name, image}: any) => {
+export type Card = {
+  id: number;
+   name: string;
+   image: string;
+ };
+
+const PetsCard = ({name, image, id}: Card) => {
   return (
-
-        <div >
-        <Image src={image} alt='card image' width={300} height={300}></Image>
+        <div className='h-60 w-80 border rounded-xl' key={id}>
+        <Image src={image} alt='card image' width={306} height={280}></Image>
+        <div className='flex justify-between border rounded-xl bg-slate-300 h-20'>
         <h2 className='font-bold text-2xl'>{name}</h2>
-        <div>
-         <h3>name</h3>
+        <p className='w-8 h-8 border rounded-full bg-slate-200 flex justify-center items-center text-amber-600'>
+        <FaArrowRight />
+        </p>
         </div>
      </div>
   
