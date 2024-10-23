30edbe123f6a631251eadcfdcb1d8d86e6fcc4e9
Dwjrgal <dwjrgl651@gmail.com>
2024-10-23 12:19:29 +0800

    news

---
+++ frontend/src/components/main_page/news_blogs.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewsAndBlogs = () => {
+  return (
+    <section>
+      <h2>News & Blogs</h2>
+    </section>
+  );
+};
+
+export default NewsAndBlogs;
--- frontend/src/lib/data.ts
+++ frontend/src/lib/data.ts
@@ -1,29 +1,42 @@
 export type Card = {
-   id: number;
-    name: string;
-    image: string;
-  };
-  
-  export const Cards: Card[] = [
-    {
-      id: 1,
-      name: "Fluffy",
-      image: "https://i.pinimg.com/736x/83/d4/f2/83d4f2a4da3cef91bbc4de211e7e6d0f.jpg",
-    },
-    {
-      id: 2,
-      name: "Spot",
-      image: "https://i.pinimg.com/enabled/736x/ab/87/04/ab87043f15a845cd1608e3085d710ce4.jpg",
-    },
-    {
-      id: 3,
-      name: "Pet name",
-      image: "https://i.pinimg.com/enabled/564x/61/2c/aa/612caa88fc7b7a9b3a0390dac6f9e77d.jpg",
-    },
-    {
-      id: 4,
-      name: "Pet name",
-      image: "https://i.pinimg.com/enabled/736x/27/f5/b4/27f5b487911392cf1e73f83498107006.jpg",
-    },
-  ];
-  
+  id: number;
+  name: string;
+  image: string;
+};
+
+export const Cards: Card[] = [
+  {
+    id: 1,
+    name: "Fluffy",
+    image:
+      "https://i.pinimg.com/736x/83/d4/f2/83d4f2a4da3cef91bbc4de211e7e6d0f.jpg",
+  },
+  {
+    id: 2,
+    name: "Spot",
+    image:
+      "https://i.pinimg.com/enabled/736x/ab/87/04/ab87043f15a845cd1608e3085d710ce4.jpg",
+  },
+  {
+    id: 3,
+    name: "Pet name",
+    image:
+      "https://i.pinimg.com/enabled/564x/61/2c/aa/612caa88fc7b7a9b3a0390dac6f9e77d.jpg",
+  },
+  {
+    id: 4,
+    name: "Pet name",
+    image:
+      "https://i.pinimg.com/enabled/736x/27/f5/b4/27f5b487911392cf1e73f83498107006.jpg",
+  },
+];
+
+export const NewsBlogs = [
+  {
+    id: 1,
+    date: "24 May,2024",
+    image:
+      "https://i.pinimg.com/736x/83/d4/f2/83d4f2a4da3cef91bbc4de211e7e6d0f.jpg",
+    title: "Urna cras et mauris congue nunc nisi nec tempus cursus",
+  },
+];
