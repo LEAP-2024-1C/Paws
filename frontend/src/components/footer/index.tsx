24508eb66d2729b362aa7443947b0b8dbf1799a1
Цэгмэд Туул <24LP5199@G303-CC0147.local>
2024-10-23 11:42:48 +0800

    header, footer

---
+++ frontend/src/components/footer/index.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { IoIosCall } from "react-icons/io";
+import { IoIosMail } from "react-icons/io";
+const Footer = () => {
+  return (
+    <footer className="flex justify-around p-6 bg-slate-200 ">
+      <div>
+        <Link href="/" className="flex gap-4">
+          <img src="/images/logo.png" alt="logo" />
+          <p>Pawchig</p>
+        </Link>
+      </div>
+      <ul className="flex  gap-6">
+        <Link href="/"> Нүүр хуудас</Link>
+        <Link href="/adoption"> Үрчлэл</Link>
+        <Link href="/donation"> Хандив</Link>
+        <Link href="/sos">Яаралтай тусламж</Link>
+        <Link href="/blog">Блог</Link>
+      </ul>
+
+      {/* <ul className='flex  gap-6'>
+            <FaFacebook />
+            <FaInstagram />
+            <FaTwitter />
+            <FaLinkedin />
+            </ul> */}
+      <div>
+        <span className="flex gap-6">
+          <IoIosCall />
+          <p>(976) 7007-1234</p>
+        </span>
+        <span className="flex gap-6">
+          <IoIosMail />
+          <p>contact@pawchig.mn</p>
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
