c8ef5b81f54ae5bac1c9b0ee75229dbfe2834184
Tuul <tuul@Tuuls-MacBook-Air.local>
2024-10-24 11:35:02 +0800

    donation test

--- frontend/src/app/donation/page.tsx
+++ frontend/src/app/donation/page.tsx
@@ -1,23 +1,26 @@
 "use client";
-import Hero from "@/components/donation_section/hero";
+
 import DonationCard from "@/components/donation_section/donation_card";
 import React from "react";
 import Link from "next/link";
+import DonationHero from "@/components/donation_section/hero";
 
 const Donation = () => {
   const mockData = ["1", "2", "3", "4", "5", "6", "7", "8"];
   return (
     <div>
-      <Hero />
-      <Link href="/donation_detail">
-        <div className="grid grid-cols-4 w-5/6 mx-auto my-5 gap-6">
-          {mockData?.map((c, i) => (
-            <div className="col-span-1 row-span-1">
-              <DonationCard />
-            </div>
-          ))}
-        </div>
-      </Link>
+      <DonationHero />
+      <div className="py-10">
+        <Link href="/donation_detail">
+          <div className="grid grid-cols-3 w-3/5 mx-auto my-5 gap-6">
+            {mockData?.map((c, i) => (
+              <div className="col-span-1 row-span-1">
+                <DonationCard />
+              </div>
+            ))}
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
