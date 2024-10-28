d3d9141aaba32f68800697853774c7f9b14a3817
Myeruyert <myeruyertjanibyek@gmail.com>
2024-10-28 09:03:28 +0800

    adoption schema

---
+++ backend/src/models/adoption/adoption.model.ts
@@ -0,0 +1,52 @@
+import { model, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+interface IAdoption {
+  _id: Schema.Types.ObjectId;
+  petId: Schema.Types.ObjectId;
+  userId: Schema.Types.ObjectId;
+  description: string;
+  location: string;
+  pre_adoption_checks: string;
+  status: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+const adoptionSchema = new Schema<IAdoption>({
+  petId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "PetProfile",
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    requied: true,
+  },
+  pre_adoption_checks: String,
+  status: {
+    type: String,
+    enum: ["posted", "in_progress", "adopted"],
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Adoption = model("Adoption", adoptionSchema);
+
+export default Adoption;
