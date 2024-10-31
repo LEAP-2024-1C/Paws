3fcf5a991534705237008480036a037f92c6a026
Tuul29 <tuul.tsegmed0329@gmail.com>
2024-10-31 19:10:43 +0800

    Donation update & delete

--- backend/src/controllers/donation/donation-controller.ts
+++ backend/src/controllers/donation/donation-controller.ts
@@ -46,3 +46,55 @@ export const createDonations = async (req: Request, res: Response) => {
     res.status(500).json({ message: "Server error", error });
   }
 };
+export const deleteDonationReport = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const deletedDonation = await Donations.findByIdAndDelete(id);
+    if (!deletedDonation) {
+      return res.status(404).json({ message: "Donation report not found" });
+    }
+    res.status(200).json({
+      message: "Deleted Donation report successfully",
+      deletedDonation,
+    });
+  } catch (error) {
+    console.error("Error deleting donation report:", error);
+    res.status(500).json({ message: "Failed to delete Donation report" });
+  }
+};
+// export const updateDonation = async (req: Request, res: Response) => {
+//   const { id } = req.params;
+//   try {
+//     const report = await Donations.findByIdAndUpdate(id, { new: true });
+
+//     if (!report) {
+//       return res.status(404).json({ message: "Report not found" });
+//     }
+
+//     res.status(200).json(report);
+//   } catch (error) {
+//     res.status(500).json({ message: "Failed to update report status" });
+//   }
+// };
+
+export const updateDonation = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const updateData = req.body; // Шинэ өгөгдлийг авах
+
+  try {
+    const report = await Donations.findByIdAndUpdate(id, updateData, {
+      new: true,
+    });
+
+    // Устгасан мэдээлэл олдсон эсэхийг шалгах
+    if (!report) {
+      return res.status(404).json({ message: "Report not found" });
+    }
+
+    res.status(200).json(report);
+  } catch (error) {
+    console.error("Error updating report:", error); // Алдааг консольд бичих
+    res.status(500).json({ message: "Failed to update report status" });
+  }
+};
