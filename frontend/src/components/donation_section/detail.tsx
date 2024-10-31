c3ac33cfcc3de92497c660083272259bb182de8b
Tuul29 <tuul.tsegmed0329@gmail.com>
2024-10-31 18:13:05 +0800

    donation front&back

--- frontend/src/components/donation_section/detail.tsx
+++ frontend/src/components/donation_section/detail.tsx
@@ -15,12 +15,14 @@ import { Progress } from "../ui/progress";
 import { DialogButton } from "./dialog";
 import Link from "next/link";
 import { PayCard } from "./pay/pay_card";
+import { DonationContext } from "../context/donation_context";
 const DetailCard = () => {
+  const { oneDonationPost } = React.useContext(DonationContext);
   return (
     <Card className=" xl:w-3/6 bg-white border-none rounded-xl p-3">
       <CardHeader>
         <img
-          src="/images/cat.png"
+          src={oneDonationPost.images}
           alt=""
           className="w-full left-60 border-0 rounded-lg"
         />
@@ -35,21 +37,9 @@ const DetailCard = () => {
 
       <CardContent>
         <h1 className="text-lg md:text-2xl font-semibold mb-4">
-          Every five minutes, someone calls us to report an
+          {oneDonationPost.title}
         </h1>
-        <p className="text-sm md:text-lg">
-          As the days grow longer, animal cruelty has fewer places to hide. And
-          in summer, the number of cases reaches a terrible peak.  No animal
-          deserves heartbreaking abuse. Animals are like us: they feel
-          desperation, confusion and terror. They feel every punch, every broken
-          bone and every burn.  That’s why, together, we must stand against
-          cruelty and continue to be there for the animals who desperately need
-          our help now. To rescue them from harm and give them the lives they
-          deserve – free from terror and torment and full of care and kindness.
-          I'm raising money for Perfect Pets Rescue and your contribution will
-          make an impact, whether you donate $5 or $500. Every little bit helps.
-          Thank you for your support.
-        </p>
+        <p className="text-sm md:text-lg">{oneDonationPost.description}</p>
       </CardContent>
       <CardFooter className="flex justify-between">
         <div className="flex gap-2">
@@ -62,10 +52,9 @@ const DetailCard = () => {
     </Card>
   );
 };
-
 export default DetailCard;
-
 export function DonationPay() {
+  const { oneDonationPost } = React.useContext(DonationContext);
   return (
     <div className="mx-auto flex flex-col gap-4 sm:w-4/5 md:3/5 md:gap-8 xl:w-2/6 xl:h-1/2 bg-white border-0 rounded-xl p-[30px] ">
       <CardTitle className="text-xl md:text-3xl xl:text-5xl font-bold">
@@ -75,12 +64,12 @@ export function DonationPay() {
         <Progress value={33} />
       </span>
       <CardTitle className="flex gap-3">
-        <p className="text-lg xl:text-2xl">100$</p>
+        <p className="text-lg xl:text-2xl">{oneDonationPost.totalAmount}$</p>
         <p className="text-xs xl:text-lg">raised out of</p>
         <p className="text-sm xl:text-xl">43$</p>
       </CardTitle>
 
-
+      <PayCard />
       <Link href="/donation">
         <Button className="w-full mx-auto border-2 border-orange-400 bg-white text-black ">
           View other Donate
--- frontend/src/components/donation_section/donation_card.tsx
+++ frontend/src/components/donation_section/donation_card.tsx
@@ -10,51 +10,67 @@ import {
 } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Progress } from "../ui/progress";
-const DonationCard = () => {
+import Link from "next/link";
+
+export type donationPostsProps = {
+  title: string;
+  description: string;
+  images: string;
+  totalAmount: number;
+  _id: string;
+  updateDate: number;
+};
+const DonationCard = ({
+  title,
+  description,
+  _id,
+  images,
+  totalAmount,
+  updateDate,
+}: donationPostsProps) => {
   return (
-    <Card className="m-auto w-3/4 2xl:w-[400px] relative p-2 flex flex-col lg:gap-3">
-      <CardHeader className="mb-4 p-0 h-48">
-        <div className="overflow-hidden rounded-t-lg">
-          <img
-            src="/images/donation.png"
-            alt="Sample Image"
-            className="rounded-[20px] object-fill scale-105"
-          />
-        </div>
-      </CardHeader>
-      <CardContent className="flex gap-2">
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-        <p className="text-sm lg:text-lg font-bold">Aztai sawar</p>
-      </CardContent>
+    <Link href={"/donation/" + _id}>
+      <Card className="m-auto w-3/4 2xl:w-[400px] relative p-2 flex flex-col lg:gap-3">
+        <CardHeader className="mb-4 p-0 h-48">
+          <div className="overflow-hidden rounded-t-lg">
+            <img
+              src={images}
+              alt="Sample Image"
+              className="rounded-[20px] object-fill scale-105"
+            />
+          </div>
+        </CardHeader>
+        <CardContent className="flex gap-2">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <p className="text-sm lg:text-lg font-bold">Aztai sawar</p>
+        </CardContent>
 
-      <CardContent>
-        {/* <h1 className="text-2xl mb-2 font-semibold text-[#FD7E14]">Pet Name</h1> */}
-        <p className="text-justify">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur,
-          modi.
-        </p>
-      </CardContent>
+        <CardContent>
+          {/* <h1 className="text-2xl mb-2 font-semibold text-[#FD7E14]">Pet Name</h1> */}
+          <p className="text-justify">{title}</p>
+        </CardContent>
 
-      <CardTitle className="flex gap-1 lg:gap-3">
-        <p className="text-lg lg:text-2xl">100$</p>
-        <p className="text-sm lg:text-lg">raised out of</p>
-        <p className="text-sm lg:text-xl">43$</p>
-      </CardTitle>
-      <span>
-        <Progress value={33} />
-      </span>
-      <CardFooter className="flex justify-between">
-        <div className="flex gap-1 lg:gap-2">
-          <FaHeart />
-          <p className="text-sm 2xl:text-base">25</p>
-          <p className="text-sm 2xl:text-base">Contributors</p>
-        </div>
-        <p>29 March, 2024</p>
-      </CardFooter>
-    </Card>
+        <CardTitle className="flex gap-1 lg:gap-3">
+          <p className="text-lg lg:text-2xl">{totalAmount}$</p>
+          <p className="text-sm lg:text-lg">raised out of</p>
+          <p className="text-sm lg:text-xl">43$</p>
+        </CardTitle>
+        <span>
+          <Progress value={33} />
+        </span>
+        <CardFooter className="flex justify-between">
+          <div className="flex gap-1 lg:gap-2">
+            <FaHeart />
+            <p className="text-sm 2xl:text-base">25</p>
+            <p className="text-sm 2xl:text-base">Contributors</p>
+          </div>
+          <p>{updateDate}</p>
+        </CardFooter>
+      </Card>
+    </Link>
   );
 };
 
