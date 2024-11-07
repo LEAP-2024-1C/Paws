8f1ac2ccabb8a783aaf15040cfcaec56b9b69a0e
Dwjrgal <dwjrgl651@gmail.com>
2024-11-07 10:43:10 +0800

    link products detail page

--- frontend/src/components/main_page/shopping_cards.tsx
+++ frontend/src/components/main_page/shopping_cards.tsx
@@ -9,27 +9,30 @@ import {
   CardFooter,
   CardHeader,
 } from "@/components/ui/card";
+import Link from "next/link";
 
 const ShoppingCards = ({ images, name, price, _id }: IProduct) => (
-  <Card key={_id} className="border rounded-xl shadow-lg">
-    <Image
-      src={images[0]}
-      alt="Example"
-      width={1200}
-      height={800}
-      quality={100}
-      priority
-    />
-    <div className="bg-slate-50 pt-2 rounded-b-xl">
-      <div className="flex justify-between mt-5 px-5">
-        <div>
-          <h4 className="font-bold text-lg">{name}</h4>
-          <p className="pl-5 pb-5 text-sm">{price.toLocaleString()}₮</p>
+  <Link href={`../shop/${_id}`}>
+    <Card key={_id} className="border rounded-xl shadow-lg">
+      <Image
+        src={images[0]}
+        alt="Example"
+        width={1200}
+        height={800}
+        quality={100}
+        priority
+      />
+      <div className="bg-slate-50 pt-2 rounded-b-xl">
+        <div className="flex justify-between mt-5 px-5">
+          <div>
+            <h4 className="font-bold text-lg">{name}</h4>
+            <p className="pl-5 pb-5 text-sm">{price.toLocaleString()}₮</p>
+          </div>
+          <Heart color="orange" />
         </div>
-        <Heart color="orange" />
       </div>
-    </div>
-  </Card>
+    </Card>
+  </Link>
 );
 
 export default ShoppingCards;
